80bb4dc25c0ed77e14e40d5356b87094d8dcc9ac
MoonHyekyung <moonsoft02@gmail.com>
2020-05-19 20:41:28 +0900

    인스턴스 생성

--- mybook/Program.cs
+++ mybook/Program.cs
@@ -6,23 +6,25 @@ using System.Threading.Tasks;
 
 namespace mybook
 {
-    class mybook
-    {
-        string name;
-        string date;
-        string author;
-        string publisher;
-        string publisherCompany;
-        string alleditor;
-        string planner;
-        string editor;
-        string designer;
-    }
     class Program
     {
+        class Mybook
+        {
+            public string bookname;
+            public string date;
+            public string author;
+            public string publisher;
+            public string publisherCompany;
+            public string alleditor;
+            public string planner;
+            public string editor;
+            public string designer;
+        }
         static void Main(string[] args)
         {
-
+            Mybook book = new Mybook() { bookname = "PHP 프로그래밍 입문",  date = "2013년 5월 20일", 
+                author = "황재호", publisher = "김태헌", publisherCompany = "한빛아카데미(주)", 
+                alleditor = "김현용", planner = "김이화", editor = "김이화", designer = "여동일"};
         }
     }
 }
