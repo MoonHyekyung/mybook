806d975db25f023e0798fad34e74fa6d131b0f9e
MoonHyekyung <moonsoft02@gmail.com>
2020-05-19 20:32:40 +0900

    class 생성

--- mybook/Program.cs
+++ mybook/Program.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace mybook
 {
-    
+    class mybook
+    {
+        string name;
+        string date;
+        string author;
+        string publisher;
+        string publisherCompany;
+        string alleditor;
+        string planner;
+        string editor;
+        string designer;
+    }
     class Program
     {
         static void Main(string[] args)
